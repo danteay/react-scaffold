9549f4d9b61c373d26afce9b91f55221e4405ade
Eduardo Aguilar <dante.aguilar41@gmail.com>
2020-01-17 11:52:22 -0600

    add configuration file for components

--- mapp.config.js
+++ mapp.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 module.exports = {
   port: 8000,
   https: false,
--- src/index.jsx
+++ src/index.jsx
@@ -4,17 +4,4 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-// class Root extends Component {
-//   render() {
-//     return (
-//       <BrowserRouter basename="/">
-//         <App />
-//       </BrowserRouter>
-//     )
-//   }
-// }
-
 ReactDOM.render(<App />, document.getElementById('app'))
-
-// // eslint-disable-next-line no-undef
-// render(AppContainer, document.getElementById('app'));
